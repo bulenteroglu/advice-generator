7e6ce14a62c92abe33294030a7977773ba0ab7ad
Bulent <eroglu.bulent98@gmail.com>
2022-02-18 19:14:13 +0000

    feat: init nextjs project

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+import Layout from '../components/Layout/Layout';
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <div>hi</div>
+    </Layout>
+  );
+};
+
+export default Home;
